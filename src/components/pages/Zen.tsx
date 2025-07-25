8dcced138a1550706466f8c21040b37d9f262bdc
Allie <howeisallie@gmail.com>
2025-07-25 17:32:50 +0100

    added button to zen page

--- src/components/pages/Zen.tsx
+++ src/components/pages/Zen.tsx
@@ -1,1 +1,14 @@
-export const Zen = () => null;
+import PauseIcon from 'mdi-react/PauseIcon';
+import { Button } from '../Button';
+import { usePageContext } from '../../backgrounds/PageContext';
+
+export const Zen = () => {
+  const {setZenMode} = usePageContext();
+  return <div className='flex justify-center relative w-full h-screen'>
+    <Button
+      icon={PauseIcon}
+      onClick={() => setZenMode(false)}
+      className='absolute bottom-10'
+    />
+  </div>
+}
