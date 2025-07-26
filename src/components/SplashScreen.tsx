1b933fb1b03513b765ac09b6388cf845015287dd
Allie <howeisallie@gmail.com>
2025-07-26 21:28:53 +0100

    moved social links to about page

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,16 +1,7 @@
-import { NewTabLink } from './pages/About';
-import { data } from '../linkData.json';
 import { ShuffleButton } from './ShuffleButton';
 
 export const SplashScreen = () => <div>
   <p className='text-9xl'>allie.</p>
-  <div className='flex justify-between'>
-    {data.map(({logo, url}, index) => (
-      <NewTabLink key={index} href={url} className='flex justify-center gap-2 items-center text-white hover:opacity-80 duration-200'>
-        <img src={logo} className='invert w-8' />
-      </NewTabLink>
-    ))}
-  </div>
   <div className='flex justify-center items-center w-full mt-20'>
     <ShuffleButton />
   </div>
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,6 +1,7 @@
 import { ContentWrapper } from './ContentWrapper';
 import { NowPlaying } from '../NowPlaying';
 import { useBreakpoints } from '../../utils/breakpointUtils';
+import { data } from '../../linkData.json';
 
 interface LinkProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
   children: React.ReactNode
@@ -14,6 +15,13 @@ export const About = () => {
   const {md} = useBreakpoints();
   return (
     <ContentWrapper text='about'>
+      <div className='flex gap-10 my-4'>
+        {data.map(({logo, url}, index) => (
+          <NewTabLink key={index} href={url} className='flex justify-center gap-2 items-center text-white hover:opacity-80 duration-200'>
+            <img src={logo} className='invert w-8' />
+          </NewTabLink>
+        ))}
+      </div>
       <div className='flex flex-col' style={{gap: md ? '2em' : '1em'}}>
         {!md && <NowPlaying />}
         <div className='text-white font-sans flex flex-col gap-3'>
