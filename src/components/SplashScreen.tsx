b411ef56d02281ad6da51ecab9d40ff937a59f5d
Allie <howeisallie@gmail.com>
2025-07-31 15:54:36 +0100

    splash screen no longer blocks button interaction

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,6 +1,6 @@
 import { ShuffleButton } from './ShuffleButton';
 
-export const SplashScreen = () => <div className='h-screen flex flex-col justify-evenly'>
+export const SplashScreen = () => <div className='h-[90dvh] flex flex-col justify-evenly'>
   <p className='text-9xl'>allie.</p>
   <div className='flex justify-center items-center w-full mt-20'>
     <ShuffleButton />
