fb94a4706e2620fe411143e1d485c6cf251f8a61
Allie <howeisallie@gmail.com>
2025-07-31 16:31:58 +0100

    datGuiUtils now its own file

---
+++ src/utils/datGuiUtils.ts
@@ -0,0 +1,4 @@
+export function removeDatGui() {
+  const existingGui = document.querySelector('.dg.main');
+  if (existingGui) existingGui.remove();
+}
--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -6,8 +6,3 @@ export const cols = {
 export const getColDarkCss = (opacity?: number) => (
   `rgba(${cols.dark}, ${cols.dark}, ${cols.dark}${opacity ? `, ${opacity}` : ''})`
 );
-
-export function removeDatGui() {
-  const existingGui = document.querySelector('.dg.main');
-  if (existingGui) existingGui.remove();
-}
