a55fc6c0bfc5056d01bd6386389bbcb6a01ab703
Allie <howeisallie@gmail.com>
2025-07-31 17:20:11 +0100

    PlainBlack -> NoGallery

--- src/components/GalleryBackground/index.tsx
+++ src/components/GalleryBackground/index.tsx
@@ -1,11 +1,11 @@
 import { usePageContext } from '../../PageContext';
 import { Screen } from '../../utils';
 import { PageWrapper } from '../wrappers/PageWrapper';
-import { Checkers, CirclesAndLines, ContrastingCircles, Ellipses, GameOfLife, PerlinGrid, PlainBlack, RotatingPlus, RotatingTriangles, SquareClover, TwinCircles } from './animations'
+import { Checkers, CirclesAndLines, ContrastingCircles, Ellipses, GameOfLife, PerlinGrid, NoGallery, RotatingPlus, RotatingTriangles, SquareClover, TwinCircles } from './animations'
 
 // TODO: Implement lazy loading for these components? This would also help with resizing issues
 export const backgrounds = [
-  PlainBlack,
+  NoGallery,
   PerlinGrid,
   Checkers,
   Ellipses,
@@ -21,13 +21,14 @@ export const backgrounds = [
 export const GalleryBackground = () => {
   const {backgroundIndex, screen} = usePageContext();
 
-  return (
-    <div className="h-full w-full absolute overflow-hidden" style={{filter: screen === Screen.Gallery ? undefined : 'blur(2px)'}}>
-      {backgrounds.map((Component, index) => (
-        <PageWrapper display={index === backgroundIndex} key={index}>
-          <Component key={index}/>
-        </PageWrapper>
-      ))}
-    </div>
-  );
+  return <div
+    className="h-full w-full absolute overflow-hidden"
+    style={{filter: screen === Screen.Gallery ? undefined : 'blur(2px)'}}
+  >
+    {backgrounds.map((Component, index) => (
+      <PageWrapper display={index === backgroundIndex} key={index}>
+        <Component key={index}/>
+      </PageWrapper>
+    ))}
+  </div>;
 };
