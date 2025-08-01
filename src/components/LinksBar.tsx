8bc5f3f0e5af5460650bbffd22cb4eb4f78c45eb
Allie <howeisallie@gmail.com>
2025-08-01 17:01:32 +0100

    code cleanup

--- src/components/LinksBar.tsx
+++ src/components/LinksBar.tsx
@@ -3,19 +3,18 @@ import { defaultTransition } from '../App'
 import { getDarkCss, Screen } from '../utils'
 import { pinks } from './buttons/Button'
 
+const screensMap = {
+  about: Screen.About,
+  projects: Screen.Projects,
+};
+
 export const LinksBar = () => {
   const {screen, setScreen, showGallery, fullscreen} = usePageContext()
 
   const homeScreen = showGallery ? Screen.Gallery : Screen.Splash;
   const opacity = fullscreen ? 'opacity-0' : 'opacity-100';
 
-  const screensMap = {
-    about: Screen.About,
-    projects: Screen.Projects,
-  }
-
-  return <div
-    className={`${opacity} ${defaultTransition} flex justify-around w-full text-xl`}
+  return <div className={`${opacity} ${defaultTransition} flex justify-around w-full text-xl`}
     style={{
       backgroundColor: getDarkCss(0.9),
       boxShadow: `0 0 100px 100px ${getDarkCss(0.9)}`,
@@ -24,10 +23,9 @@ export const LinksBar = () => {
     {/* TODO: Sweeping pink boxes when tab is selected */}
     {/* TODO: Re-evaluate colors - maybe have these white, page headers pink and page content white? */}
     {Object.entries(screensMap).map(([name, thisScreen]) => (
-      <p
+      <p className={`${defaultTransition} px-2 pt-0.5 pb-5 cursor-pointer`}
         key={name}
         onClick={() => setScreen(screen !== thisScreen ? thisScreen : homeScreen)}
-        className={`${defaultTransition} px-2 pt-0.5 pb-5 cursor-pointer`}
         style={{
           backgroundColor: screen === thisScreen ? pinks[0] : undefined,
           color: screen === thisScreen ? 'black' : undefined
