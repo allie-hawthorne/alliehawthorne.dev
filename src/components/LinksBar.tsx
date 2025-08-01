39dfd4d322f3b8ec908c02eb65357b273e0f278f
Allie <howeisallie@gmail.com>
2025-08-01 17:55:37 +0100

    ScreenLink now its own component

--- src/components/LinksBar.tsx
+++ src/components/LinksBar.tsx
@@ -8,7 +8,7 @@ const screensMap = {
 };
 
 export const LinksBar = () => {
-  const {screen, setScreen, showGallery, fullscreen} = usePageContext()
+  const {showGallery, fullscreen} = usePageContext()
 
   const homeScreen = showGallery ? Screen.Gallery : Screen.Splash;
   const opacity = fullscreen ? 'opacity-0' : 'opacity-100';
@@ -19,19 +19,29 @@ export const LinksBar = () => {
       boxShadow: `0 0 100px 100px ${getDarkCss(0.9)}`,
     }}
   >
-    {Object.entries(screensMap).map(([name, thisScreen]) => {
-      const screenActive = screen === thisScreen;
-      const textColour = screenActive ? 'text-black' : undefined
-
-      return <button className={`${defaultTransition} ${textColour} px-2 pt-0.5 pb-5 cursor-pointer overflow-hidden relative`}
-        key={name}
-        onClick={() => setScreen(screenActive ? homeScreen : thisScreen)}
-      >
-        <div className={`absolute inset-0 bg-pink-300 origin-bottom ${defaultTransition} ${screenActive ? 'scale-y-100' : 'scale-y-0'}`} />
-        {/* This is dumb but just having one element with position: absolute deprives the button of its width */}
-        <p className='opacity-0' aria-hidden>{name}</p>
-        <p className='absolute inset-0'>{name}</p>
-      </button>
-    })}
+    {Object.entries(screensMap).map((s, i) => (
+      <ScreenLink key={i} screenData={s} homeScreen={homeScreen} />
+    ))}
   </div>;
+}
+
+interface ScreenLinkProps {
+  screenData: [string, Screen]
+  homeScreen: Screen
+}
+const ScreenLink = ({homeScreen, screenData: screenData}: ScreenLinkProps) => {
+  const [name, thisScreen] = screenData;
+  const {screen, setScreen} = usePageContext()
+
+  const isScreenActive = screen === thisScreen;
+  const textColour = isScreenActive ? 'text-black' : undefined;
+
+  const changeActiveScreen = () => setScreen(isScreenActive ? homeScreen : thisScreen);
+
+  return <button className={`${defaultTransition} ${textColour} px-2 pt-0.5 pb-5 cursor-pointer overflow-hidden relative`} onClick={changeActiveScreen}>
+    <div className={`absolute inset-0 bg-pink-300 origin-bottom ${defaultTransition} ${isScreenActive ? 'scale-y-100' : 'scale-y-0'}`} />
+    {/* This is dumb but just having one element with position: absolute deprives the button of its width */}
+    <p className='opacity-0' aria-hidden>{name}</p>
+    <p className='absolute inset-0'>{name}</p>
+  </button>
 }
