e2bcc41c5bd23120b0a2f043a6ea75c70da3b9b2
Allie <howeisallie@gmail.com>
2025-08-04 11:09:54 +0100

    redesigned modal to animate it, added logic to hide it

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -1,9 +1,25 @@
 import { usePageContext } from '../PageContext'
+import { defaultTransition } from '../App';
+import { useEffect, useState } from 'react';
+import { zIndices } from '../utils/zIndices';
 
 export const AboutModal = () => {
-  const {showModal} = usePageContext();
+  const {showModal, setShowModal} = usePageContext();
 
-  return <dialog open={showModal}>
-    <p>coming soon....</p>
-  </dialog>
+  const [styles, setStyles] = useState('invisible opacity-0');
+
+  // TODO: Share all of these where possible
+  useEffect(() => {
+    setStyles(showModal
+      ? ''
+      : 'invisible opacity-0'
+    );
+  }, [showModal]);
+
+  // TODO: Try out using <dialog> here (I couldn't get transition/background to work nicely)
+  return <div className={`${defaultTransition} ${styles} ${zIndices.aboutModal} bg-opacity-80 absolute inset-0 bg-black flex justify-center items-center`} onClick={() => setShowModal(false)}>
+    <div>
+      <p>coming soon....</p>
+    </div>
+  </div>;
 }
