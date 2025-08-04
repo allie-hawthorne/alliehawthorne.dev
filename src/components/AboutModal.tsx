eb07265e2816a3dceb6b347112b5c035807f73a3
Allie <howeisallie@gmail.com>
2025-08-04 12:38:44 +0100

    better styling for about modal

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -2,6 +2,7 @@ import { usePageContext } from '../PageContext'
 import { defaultTransition } from '../App';
 import { useEffect, useState } from 'react';
 import { zIndices } from '../utils/zIndices';
+import { getDarkCss } from '../utils';
 
 export const AboutModal = () => {
   const {showModal, setShowModal} = usePageContext();
@@ -17,9 +18,13 @@ export const AboutModal = () => {
   }, [showModal]);
 
   // TODO: Try out using <dialog> here (I couldn't get transition/background to work nicely)
-  return <div className={`${defaultTransition} ${styles} ${zIndices.aboutModal} bg-opacity-80 absolute inset-0 bg-black flex justify-center items-center`} onClick={() => setShowModal(false)}>
-    <div>
-      <p>coming soon....</p>
+  return <div className={`${defaultTransition} ${styles} ${zIndices.aboutModal} absolute inset-0 flex justify-center items-end p-16`} onClick={() => setShowModal(false)}>
+    <div className='flex flex-col gap-2 justify-center rounded-lg w-64 p-2 aspect-video text-white' style={{backgroundColor: getDarkCss(), boxShadow: `0 0 20px #FFF6`}}>
+      <div>
+        <p>circles and lines</p>
+        <p className='text-pink-300'>2019</p>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, officiis!</p>
     </div>
   </div>;
 }
