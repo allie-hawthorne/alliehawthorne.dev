03972fc567d4633106ed599d299e88e4fa22a545
Allie <howeisallie@gmail.com>
2025-08-04 12:47:08 +0100

    extracting modal text into JSON object

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -1,12 +1,12 @@
+import { useEffect, useState } from 'react';
+import galleryData from '../galleryInfo.json'
 import { usePageContext } from '../PageContext'
 import { defaultTransition } from '../App';
-import { useEffect, useState } from 'react';
 import { zIndices } from '../utils/zIndices';
 import { getDarkCss } from '../utils';
 
 export const AboutModal = () => {
-  const {showModal, setShowModal} = usePageContext();
-
+  const {backgroundIndex, showModal, setShowModal} = usePageContext();
   const [styles, setStyles] = useState('invisible opacity-0');
 
   // TODO: Share all of these where possible
@@ -17,14 +17,19 @@ export const AboutModal = () => {
     );
   }, [showModal]);
 
+  const itemData = galleryData[backgroundIndex - 1]
+  if (!itemData) return null;
+
+  const {description, name, year} = itemData
+
   // TODO: Try out using <dialog> here (I couldn't get transition/background to work nicely)
   return <div className={`${defaultTransition} ${styles} ${zIndices.aboutModal} absolute inset-0 flex justify-center items-end p-16`} onClick={() => setShowModal(false)}>
     <div className='flex flex-col gap-2 justify-center rounded-lg w-64 p-2 aspect-video text-white' style={{backgroundColor: getDarkCss(), boxShadow: `0 0 20px #FFF6`}}>
       <div>
-        <p>circles and lines</p>
-        <p className='text-pink-300'>2019</p>
+        <p>{name}</p>
+        <p className='text-pink-300'>{year}</p>
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, officiis!</p>
+      <p>{description}</p>
     </div>
   </div>;
 }
