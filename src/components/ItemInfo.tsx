e973ec59307274ab422c6b7b7525c314ec88edcf
Allie <me@alliehawthorne.dev>
2026-06-07 16:07:53 +0100

    added filtering by tag

--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -1,20 +1,39 @@
-import { PropsWithChildren } from "react";
+import { Dispatch, HTMLAttributes, PropsWithChildren, SetStateAction } from "react";
 
-interface ItemInfoProps {
+export interface TagState {
+  filterTag: string
+  setFilterTag: Dispatch<SetStateAction<string>>
+}
+
+interface TagsProps extends TagState {
+  tags: string[]
+}
+
+interface BaseItemInfoProps {
   name: string
   year: number
-  tags: string[]
   description: string[]
 }
-export const ItemInfo = ({description, name, year, tags}: ItemInfoProps) => <div className="flex flex-col gap-2">
+
+type ItemInfoProps = BaseItemInfoProps | (BaseItemInfoProps & TagsProps)
+export const ItemInfo = ({description, name, year, ...tagState}: ItemInfoProps) => <div className="flex flex-col gap-2">
   <div className='flex justify-between items-center'>
     <p className='text-2xl'>{name}</p>
     <p className='font-sans text-white opacity-80 italic'>{year}</p>
   </div>
   <div className="flex gap-2 whitespace-nowrap overflow-auto">
-    {tags.map((tag, i) => <Tag key={i}>{tag}</Tag>)}
+    {'tags' in tagState && tagState.tags.map((tag, i) => <ProjectTag key={i} {...tagState} tag={tag}>
+    </ProjectTag>)}
   </div>
   {description.map((paragraph, i) => <p className='text-white font-sans' key={i}>{paragraph}</p>)}
 </div>
 
-const Tag = ({children}: PropsWithChildren) => <p className="bg-pink-300/80 text-black text-xs rounded-xl px-2 py-0.5">{children}</p>;
+const ProjectTag = ({filterTag, setFilterTag, tag}: TagState & {tag: string}) => {
+  const isSelected = filterTag === tag;
+  const bgColor = isSelected ? 'bg-pink-400' : 'bg-pink-300';
+  return <Tag className={bgColor} onClick={() => setFilterTag(isSelected ? '' : tag)}>{tag}</Tag>
+}
+
+const Tag = ({children, className, ...props}: PropsWithChildren<HTMLAttributes<HTMLParagraphElement>>) => (
+  <div className={`text-black text-xs rounded-xl px-2 py-0.5 cursor-pointer opacity-80 hover:opacity-60 duration-300 whitespace-nowrap ${className}`} {...props}>{children}</div>
+);
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,23 +1,35 @@
-import projects from '../../projects.json'
-import { ItemInfo } from '../ItemInfo';
+import { useMemo, useState } from 'react';
+import { ItemInfo, TagState } from '../ItemInfo';
 import { ContentWrapper } from '../wrappers/ContentWrapper';
 import { NewTabLink } from './About';
+import projects from '../../projects.json'
 
 type Project = typeof projects[0];
 
-export const Projects = () => <ContentWrapper text='projects'>
-  <div className='grid gap-20 grid-cols-1 flex-col'>
-    {projects.map((project, index) => <Project key={index} project={project} />)}
-  </div>
-</ContentWrapper>;
+export const Projects = () => {
+  const [filterTag, setFilterTag] = useState('');
+
+  const filteredProjects = useMemo(() => {
+    if (!filterTag) return projects;
+    return projects.filter(p => p.tags.includes(filterTag));
+  }, [filterTag]);
+  
+  return <ContentWrapper text='projects'>
+    <div className='grid gap-20 grid-cols-1 flex-col'>
+      {filteredProjects.map((project, index) => (
+        <Project key={index} project={project} filterTag={filterTag} setFilterTag={setFilterTag} />
+      ))}
+    </div>
+  </ContentWrapper>;
+}
 
 // TODO: Downscale images to improve performance
 // TODO: Hard-code width of images to reduce layout shift
-const Project = ({ project }: {project: Project}) => <div className='flex flex-col md:flex-row gap-x-5 gap-y-2 growOnHoverWrap'>
+const Project = ({ project, setFilterTag, filterTag }: TagState & {project: Project}) => <div className='flex flex-col md:flex-row gap-x-5 gap-y-2 growOnHoverWrap'>
   <img loading="lazy" className='md:w-[400px] rounded-xl growOnHoverItem duration-200' src={`images/previews/${project.imgUrl}`} />
   <div className='flex flex-col gap-5 justify-between'>
     <div className='w-full'>
-      <ItemInfo {...project} />
+      <ItemInfo {...project} filterTag={filterTag} setFilterTag={setFilterTag} />
     </div>
     {project.buttons.map((button, index) => (
       <NewTabLink className='rounded-md text-lg bg-pink-300 hover:bg-pink-400 duration-500 p-1 block text-center' style={{color: 'black'}} key={index} href={button.url}>
