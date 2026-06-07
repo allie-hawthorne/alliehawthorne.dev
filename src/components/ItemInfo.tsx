419000ffd6cafdfe106a1ffa02ff9e031594902d
Allie <me@alliehawthorne.dev>
2026-06-07 14:37:16 +0100

    added tags to projects

--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -1,12 +1,20 @@
+import { PropsWithChildren } from "react";
+
 interface ItemInfoProps {
   name: string
   year: number
+  tags: string[]
   description: string[]
 }
-export const ItemInfo = ({description, name, year}: ItemInfoProps) => <>
+export const ItemInfo = ({description, name, year, tags}: ItemInfoProps) => <div className="flex flex-col gap-2">
   <div className='flex justify-between items-center'>
     <p className='text-2xl'>{name}</p>
     <p className='font-sans text-white opacity-80 italic'>{year}</p>
   </div>
+  <div className="flex gap-2 whitespace-nowrap overflow-auto">
+    {tags.map((tag, i) => <Tag key={i}>{tag}</Tag>)}
+  </div>
   {description.map((paragraph, i) => <p className='text-white font-sans' key={i}>{paragraph}</p>)}
-</>;
+</div>
+
+const Tag = ({children}: PropsWithChildren) => <p className="bg-pink-300/80 text-black text-xs rounded-xl px-2 py-0.5">{children}</p>;
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -13,18 +13,16 @@ export const Projects = () => <ContentWrapper text='projects'>
 
 // TODO: Downscale images to improve performance
 // TODO: Hard-code width of images to reduce layout shift
-const Project = ({ project }: {project: Project}) => (
-  <div className='flex flex-col md:flex-row gap-x-5 gap-y-2 growOnHoverWrap'>
-    <img loading="lazy" className='md:w-[400px] rounded-xl growOnHoverItem duration-200' src={`images/previews/${project.imgUrl}`} />
-    <div className='flex flex-col gap-5 justify-between'>
-      <div className='w-full'>
-        <ItemInfo {...project} />
-      </div>
-      {project.buttons.map((button, index) => (
-        <NewTabLink className='rounded-md text-lg bg-pink-300 hover:bg-pink-400 duration-500 p-1 block text-center' style={{color: 'black'}} key={index} href={button.url}>
-          {button.title}
-        </NewTabLink>
-      ))}
+const Project = ({ project }: {project: Project}) => <div className='flex flex-col md:flex-row gap-x-5 gap-y-2 growOnHoverWrap'>
+  <img loading="lazy" className='md:w-[400px] rounded-xl growOnHoverItem duration-200' src={`images/previews/${project.imgUrl}`} />
+  <div className='flex flex-col gap-5 justify-between'>
+    <div className='w-full'>
+      <ItemInfo {...project} />
     </div>
+    {project.buttons.map((button, index) => (
+      <NewTabLink className='rounded-md text-lg bg-pink-300 hover:bg-pink-400 duration-500 p-1 block text-center' style={{color: 'black'}} key={index} href={button.url}>
+        {button.title}
+      </NewTabLink>
+    ))}
   </div>
-);
+</div>;
